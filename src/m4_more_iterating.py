0b87c36a598c9f0884d3c450ef47b15f33441fd5
JingyiJia <jiaj@rose-hulman.edu>
2019-01-06 16:51:14 -0500

    Done

--- src/m4_more_iterating.py
+++ src/m4_more_iterating.py
@@ -429,7 +429,7 @@ def count_items_bigger_than(numbers, threshold):
 def run_test_count_positive_sines():
     """ Tests the   count_positive_sines   function. """
     # -------------------------------------------------------------------------
-    # TODO: 6. Implement this TEST function.
+    # DONE: 6. Implement this TEST function.
     #   It TESTS the  count_positive_sines  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond what we supplied.
     #
